ed5af3b5972de6cf663e7ac3bf7681a5b1e8bc76
SakibulSetu <sakibul.setu57@gmail.com>
2025-01-08 23:07:23 +0600

    3rd commit

--- includes/navbarhome.php
+++ includes/navbarhome.php
@@ -1,7 +1,7 @@
 <?php
 if (!isset($_SESSION['user'])) {
     echo "
-    <nav class='navbar navbar-expand-sm bg-info navbar-dark sticky-top'>
+    <nav class='navbar navbar-expand-md bg-info navbar-dark'>
         <a class='navbar-brand animate__animated animate__heartBeat animate__slower animate__infinite' href='index'>e-ticket.com</a>
 
         <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#collapsibleNavbar'>
@@ -13,8 +13,8 @@ if (!isset($_SESSION['user'])) {
                     <a class='nav-link dropdown-toggle' href='#' id='navbardrop' data-toggle='dropdown'>Book Ticket</a>
                     <div class='dropdown-menu'>
                         <a class='dropdown-item' href='index.php'>Bus</a>
-                        <a class='dropdown-item' href='index.php'>Train</a>
-                        <a class='dropdown-item' href='index.php'>Air</a>
+                        <a class='dropdown-item' href='indexTrain.php'>Train</a>
+                        <a class='dropdown-item' href='indexAir.php'>Air</a>
                     </div>
                 </li>
                 <div class='nav1'>
---
+++ includes/searchformAir.php
@@ -0,0 +1,37 @@
+<br>
+<form action="indexAir.php" target="_self" enctype="multipart/form-data" method="POST" class="animate__animated animate__slideInRight animate__fast" id="searchform">
+    <div class="col">
+        <div class="form-group">
+            <label for="depart">From</label>
+            <select class="form-control" id="depart" name="from" required>
+                <option value="">SELECT DEPARTURE CITY</option>
+                <?php
+                $query = "SELECT DISTINCT DEPART FROM busschedule ORDER BY DEPART";
+                $result = mysqli_query($con, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    $depart = $row['DEPART'];
+                    echo "<option value='$depart'>$depart</option>";
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <div class="col">
+        <div class="form-group">
+            <label for="dest">To</label>
+            <select class="form-control" id="dest" name="to" required>
+                <option value="">SELECT DESTINATION CITY</option>
+                <?php
+                $query = "SELECT DISTINCT DEST FROM busschedule ORDER BY DEST";
+                $result = mysqli_query($con, $query);
+                while ($row = mysqli_fetch_array($result)) {
+                    $dest = $row['DEST'];
+                    echo "<option value='$dest'>$dest</option>";
+                }
+                ?>
+            </select>
+        </div>
+    </div>
+    <br><button type="submit" class="btn btn-outline-primary" value="SEARCH" name="search">SEARCH Air Tickets</button><br>
+</form>
+<br>
